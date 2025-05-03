12cfc8e3007b4e37cbd943706308603db330ba28
luyuting <705621208@qq.com>
2025-05-03 23:50:43 +0800

    fix:new页面改成非TabBar页面

--- src/pages/new/index.vue
+++ src/pages/new/index.vue
@@ -4,22 +4,15 @@
     <view class="content">
       这是新建内容
     </view>
-    
   </view>
-  <custom-tab-bar />
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import CustomTabBar from '../../custom-tab-bar/index.vue'
 
 export default defineComponent({
-  name: 'Index',
-  components: {
-    CustomTabBar
-  }
+  name: 'Index'
 })
 </script>
-<style>
 
-</style>
+<style></style>
